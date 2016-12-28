5f4cd8cf90ef4131b8cab83c55fd47933dc90eec
Kurt Lidl <lidl@pix.net>
2016-12-28 00:37:40 -0500

    Cleanup whitespace

--- Src/Include/tanktypes.h
+++ Src/Include/tanktypes.h
@@ -13,29 +13,31 @@
 #ifndef _TANKTYPES_H_
 #define _TANKTYPES_H_
 
-  typedef enum {
-	  False = 0, True = 1
-  } Boolean;
+typedef enum {
+	False = 0,
+	True = 1
+} Boolean;
 
-#define FALSE ((int)False)		/* lint likes these better :-) */
-#define TRUE ((int)True)
+#define FALSE ((int) False)		/* lint likes these better :-) */
+#define TRUE ((int) True)
 typedef unsigned char Byte;
 typedef unsigned int Flag;		/* a set of bits */
 typedef FLOAT Angle;
 
-  typedef struct {
-	  short x, y;
-  }
-Coord;
+typedef struct {
+	short x, y;
+} Coord;
 
-  typedef struct {
-	  long x, y;
-  }
-lCoord;
+typedef struct {
+	long x, y;
+} lCoord;
 
 /* directions of rotation */
-  typedef enum {
-	  COUNTERCLOCKWISE = -1, NO_SPIN = 0, CLOCKWISE = 1, TOGGLE = 2
-  } Spin;
+typedef enum {
+	COUNTERCLOCKWISE = -1,
+	NO_SPIN = 0,
+	CLOCKWISE = 1,
+	TOGGLE = 2
+} Spin;
 
 #endif /* _TANKTYPES_H_ */
