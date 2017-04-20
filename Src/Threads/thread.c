f5288a075d81db13ad01a02081cd6057f4d73db0
Kurt Lidl <lidl@pix.net>
2017-04-20 12:51:51 -0400

    Claim copyright on portions of Src/Threads/thread.c

--- Src/Threads/thread.c
+++ Src/Threads/thread.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 1988 Terry Donahue
+ * Portions Copyright (c) 1992,1993,1999,2017 Kurt Lidl
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
